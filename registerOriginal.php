2b560137efd4e93d090ef49374fec81e9c866d59
Asad Memon <amdeveloping0908@gmail.com>
2019-10-20 18:19:38 +0530

    Added Styles to register Page

---
+++ registerOriginal.php
@@ -0,0 +1,91 @@
+<?php 
+include("includes/class/Account.php"); 
+include("includes/class/Constants.php"); 
+include('includes/config.php');
+$account = new Account($connection);
+
+function getInputValue($value){
+    if(isset($_POST[$value])){
+        echo $_POST[$value];
+    }
+}
+
+?>
+<?php include("includes/handlers/register-handler.php"); ?>
+<?php include("includes/handlers/login-handler.php"); ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to slotify</title>
+    <link rel="stylesheet" href="assets/css/register.css">
+</head>
+<body>
+<div id="background">
+    <div id="loginContainer">
+        <div id="inputContainer">
+        <form action="register.php" method='POST' id="loginForm">
+        <h2>Login to your account</h2>
+        <p>
+        <?php echo $account->getError(Constants::$loginFailed) ?>
+        <label for="loginUsername">Username</label>
+        <input id="loginUsername" name="loginUsername" placeholder="e.g. John Doe" required type="text" autocomplete="off">
+        </p>
+        <p>
+        <label for="loginPassword">Password</label>
+        <input id="loginPassword" name="loginPassword" required type="password" placeholder="Your Password" autocomplete="off">
+        </p>
+        <button type="submit" name="loginButton">LOG IN</button>
+        </form>
+        
+        <form action="register.php" method='POST' id="registerForm">
+        <h2>Create Your Free Account</h2>
+        <p>
+        <?php echo $account->getError(Constants::$usernameLength) ?>
+        <?php echo $account->getError(Constants::$usernameTaken) ?>
+        <label for="username">Username</label>
+        <input id="username" name="username" placeholder="e.g. John Doe" value="<?php getInputValue('username'); ?>" required type="text" autocomplete="off">
+        </p>
+        <p>
+        <?php echo $account->getError(Constants::$firstnameLength) ?>
+        <label for="firstName">First name</label>
+        <input id="firstName" name="firstName" placeholder="e.g. John" value="<?php getInputValue('firstName'); ?>" required type="text" autocomplete="off">
+        </p>
+        <p>
+        <?php echo $account->getError(Constants::$lastnameLenght) ?>
+        <label for="lastName">Last name</label>
+        <input id="lastName" name="lastName" placeholder="e.g. Doe" value="<?php getInputValue('lastName'); ?>" required type="text" autocomplete="off">
+        </p>
+        <p>
+        <?php echo $account->getError(Constants::$emailInvalid) ?>
+        <?php echo $account->getError(Constants::$emailMatch) ?>
+        <?php echo $account->getError(Constants::$emailTaken) ?>
+        <label for="email">Email</label>
+        <input id="email" name="email" placeholder="e.g. doe@gmail.com" value="<?php getInputValue('email'); ?>" required type="email" autocomplete="off">
+        </p>
+        <p>
+        <label for="email2">Confirm Email</label>
+        <input id="email2" name="email2" placeholder="e.g. doe@gmail.com" value="<?php getInputValue('email2'); ?>" required type="email" autocomplete="off">
+        </p>
+        <p>
+        <?php echo $account->getError(Constants::$passwordAlpha) ?>
+        <?php echo $account->getError(Constants::$passwordLenght) ?>
+        <?php echo $account->getError(Constants::$passwordMatch) ?>
+        <label for="password">Password</label>
+        <input id="password" name="password" required type="password" placeholder="Your Password" autocomplete="off">
+        </p>
+        <p>
+        <label for="password2">Confirm Password</label>
+        <input id="password2" name="password2" required type="password" placeholder="Confirm Password" autocomplete="off">
+        </p>
+        <button type="submit" name="registerButton">SIGN UP</button>
+        </form>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
