669ae24aa20372c077608f710c3fc9943e2b3ea9
Asad Memon <amdeveloping0908@gmail.com>
2019-10-21 13:20:07 +0530

    Added control Buttons

--- index.php
+++ index.php
@@ -18,8 +18,28 @@ else{
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to Slotify!</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <h1>Welcome <?php echo $userLoggedIn; ?></h1>
+    <div id="nowPlayingBarContainer">
+      <div id="nowPlayingBar">
+             <div id="nowPlayingLeft">
+             </div>
+             <div id="nowPlayingCenter">
+                 <div class="content playerControls">
+                     <div class="buttons">
+                         <button class="controlButton shuffle" title="shuffle button"><img src="assets/images/icons/shuffle.png" alt="shuffle"></button>
+                         <button class="controlButton previous" title="previous button"><img src="assets/images/icons/previous.png" alt="previous"></button>
+                         <button class="controlButton play" title="play button"><img src="assets/images/icons/play.png" alt="play"></button>
+                         <button class="controlButton pause" title="pause button" style="display: none;"><img src="assets/images/icons/pause.png" alt="pause"></button>
+                         <button class="controlButton next" title="next button"><img src="assets/images/icons/next.png" alt="next"></button>
+                         <button class="controlButton repeat" title="repeat button"><img src="assets/images/icons/repeat.png" alt="repeat"></button>
+                     </div>
+                 </div>
+             </div>
+             <div id="nowPlayingRight">
+             </div>
+      </div>
+    </div>
 </body>
 </html>
--- register.php
+++ register.php
@@ -56,7 +56,7 @@ function getInputValue($value){
 					<p>
 						<?php echo $account->getError(Constants::$loginFailed); ?>
 						<label for="loginUsername">Username</label>
-						<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('loginUsername') ?>" required>
+						<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. johndoe" value="<?php getInputValue('loginUsername') ?>" required>
 					</p>
 					<p>
 						<label for="loginPassword">Password</label>
@@ -79,19 +79,19 @@ function getInputValue($value){
 						<?php echo $account->getError(Constants::$usernameLength) ?>
                         <?php echo $account->getError(Constants::$usernameTaken) ?>
 						<label for="username">Username</label>
-						<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username') ?>" required>
+						<input id="username" name="username" type="text" placeholder="e.g. johndoe" value="<?php getInputValue('username') ?>" required>
 					</p>
 
 					<p>
 						<?php echo $account->getError(Constants::$firstnameLength) ?>
 						<label for="firstName">First name</label>
-						<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" value="<?php getInputValue('firstName') ?>" required>
+						<input id="firstName" name="firstName" type="text" placeholder="e.g. John" value="<?php getInputValue('firstName') ?>" required>
 					</p>
 
 					<p>
 						<?php echo $account->getError(Constants::$lastnameLenght) ?>
 						<label for="lastName">Last name</label>
-						<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" value="<?php getInputValue('lastName') ?>" required>
+						<input id="lastName" name="lastName" type="text" placeholder="e.g. Doe" value="<?php getInputValue('lastName') ?>" required>
 					</p>
 
 					<p>
@@ -99,12 +99,12 @@ function getInputValue($value){
                         <?php echo $account->getError(Constants::$emailMatch) ?>
                         <?php echo $account->getError(Constants::$emailTaken) ?>
 						<label for="email">Email</label>
-						<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email') ?>" required>
+						<input id="email" name="email" type="email" placeholder="e.g. doe@gmail.com" value="<?php getInputValue('email') ?>" required>
 					</p>
 
 					<p>
 						<label for="email2">Confirm email</label>
-						<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2') ?>" required>
+						<input id="email2" name="email2" type="email" placeholder="e.g. doe@gmail.com" value="<?php getInputValue('email2') ?>" required>
 					</p>
 
 					<p>
