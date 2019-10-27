09608127a281a15026c7fc4ce7c428598e97ec0c
Asad Memon <amdeveloping0908@gmail.com>
2019-10-27 22:51:33 +0530

    Navbar Edited And Songs added to Db

--- index.php
+++ index.php
@@ -23,78 +23,10 @@ else{
     <div id="mainContainer">
 
         <div id="topContainer">
-            <div id="navBarContainer">
-                <nav class="navBar">   
-                    <a href="index.php" class="logo">
-                        <img src="assets/images/icons/logo.png" alt="">
-                    </a>
-
-                    <div class="group">
-                        <div class="navItem">
-                            <a href="search.php">Search</a>
-                        </div>
-                    </div>
-                    <div class="group">
-                    
-                    </div>
-                </nav>
-            </div>
+            <?php include "includes/navBarContainer.php"; ?>
         </div>
 
-
-
-
-        <div id="nowPlayingBarContainer">
-      <div id="nowPlayingBar">
-             <div id="nowPlayingLeft">
-                 <div class="content">
-                     <span class="albumLink">
-                         <img class="albumArtwork" src="https://static.wixstatic.com/media/35f40b_173bb8d9b17b43be8c84954447d10e3e~mv2_d_1205_1205_s_2.png/v1/fill/w_1205%2Ch_1205%2Cal_c%2Cq_90/file.jpg" alt="">
-                     </span>
-                     <div class="trackInfo">
-                         <span class="trackName">
-                             <span>Happy Birthday</span>
-                         </span>
-                         <span class="artistName">
-                             <span>Asad Memon</span>
-                         </span>
-                     </div>
-                 </div>
-             </div>
-             <div id="nowPlayingCenter">
-                 <div class="content playerControls">
-                     <div class="buttons">
-                         <button class="controlButton shuffle" title="shuffle button"><img src="assets/images/icons/shuffle.png" alt="shuffle"></button>
-                         <button class="controlButton previous" title="previous button"><img src="assets/images/icons/previous.png" alt="previous"></button>
-                         <button class="controlButton play" title="play button"><img src="assets/images/icons/play.png" alt="play"></button>
-                         <button class="controlButton pause" title="pause button" style="display: none;"><img src="assets/images/icons/pause.png" alt="pause"></button>
-                         <button class="controlButton next" title="next button"><img src="assets/images/icons/next.png" alt="next"></button>
-                         <button class="controlButton repeat" title="repeat button"><img src="assets/images/icons/repeat.png" alt="repeat"></button>
-                     </div>
-                     <div class="playbackBar">
-                        <span class="progressTime current">0.00</span>
-                        <div class="progressBar">
-                            <div class="progressBarBg">
-                                <div class="progress"></div>
-                            </div>
-                        </div>
-                        <span class="progressTime remaining">0.00</span>
-                     </div>
-                 </div>
-             </div>
-             <div id="nowPlayingRight">
-                 <div class="volumeBar">
-                     <button class="controlButton volume" title="Volume Button">
-                         <img src="assets/images/icons/volume.png" alt="volume button">
-                     </button>
-                     <div class="progressBar">
-                            <div class="progressBarBg">
-                                <div class="progress"></div>
-                            </div>
-                        </div>
-                 </div>
-             </div>
-      </div>
+            <?php include "includes/nowPlayingBarContainer.php"; ?>
     </div>
     </div>
 </body>
