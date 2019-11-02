c64f655fb548c6566a872d35b6ca6267e9163d6c
Asad Memon <amdeveloping0908@gmail.com>
2019-11-02 11:47:12 +0530

    Controlling progress bar using mouse and clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var currentPlaylist = [];
 var audioElement;
+var mouseDown = false;
 
 function formatTime(seconds){
   var time = Math.round(seconds);
@@ -17,6 +18,13 @@ function formatTime(seconds){
   return minutes + ":" + extraZero + seconds;
 }
 
+function updateTimeProgressBar(audio){
+  $(".progressTime.current").text(formatTime(audio.currentTime));
+
+  var progress = audio.currentTime / audio.duration * 100;
+  $(".playbackBar .progress").css("width", progress + "%");
+}
+
 
 function Audio() {
   this.currentlyPlaying;
@@ -25,6 +33,12 @@ function Audio() {
   this.audio.addEventListener("canplay", function(){
     let duration = formatTime(this.duration);
     $(".progressTime.remaining").text(duration);
+  });
+
+  this.audio.addEventListener("timeupdate", function(){
+    if(this.duration){
+      updateTimeProgressBar(this);
+    }
   })
 
   this.setTrack = function(track) {
@@ -38,4 +52,7 @@ function Audio() {
   this.pause = function() {
     this.audio.pause();
   };
+  this.setTime = function(seconds){
+    this.audio.currentTime = seconds;
+  }
 }
